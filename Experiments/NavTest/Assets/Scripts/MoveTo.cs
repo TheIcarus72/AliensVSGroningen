c54165e5f6d9520198a67c86d9f6f3bbcf9fae1b
Dave Bechtum <dbechtum@gmail.com>
2016-11-02 13:59:50 +0100

    Experiments

---
+++ Experiments/NavTest/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,35 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+	public float timer = 0.0f;
+	private bool timerOn = false;
+	private NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+
+	void Update() {
+		if (timerOn && timer < 4) {
+			timer += 1 * Time.deltaTime;
+		}
+		if (timer >= 4) {
+			timerOn = false;
+			timer = 0.0f;
+			agent.Resume();
+		}
+	}
+		void OnTriggerEnter (Collider other){
+			if (other.gameObject.tag == "Trafficlight") {
+				agent.Stop ();
+				timerOn = true;
+			}
+	}
+
+
+}
