d6cf787749bdeeb3c2862ca323c22964e7dbe836
Dave Bechtum <dbechtum@gmail.com>
2016-11-07 12:07:15 +0100

    Raycast script to detect objects infront of the camera

---
+++ Experiments/Raycast/Assets/Scripts/RayCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		Debug.DrawRay(Camera.main.transform.position,Camera.main.transform.TransformDirection(Vector3.forward));
+		Vector3 fwd = Camera.main.transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast(Camera.main.transform.position, fwd, 500)){
+			print("There is something in front of the camera!");
+		}
+		else
+		{
+			print("Move along people, nothing to see here!");
+		}
+	}
+}
