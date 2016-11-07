822c052dc1146412c5d8d8cc4173a9c219f29b0e
Dave Bechtum <dbechtum@gmail.com>
2016-11-07 13:09:11 +0100

    Camera can freely look around with the mouse, when looking at a "selectable building" and pressing space, the camera will no longer be able to move, so that the player can freely select an option from the menu(starting work on menu now) This is a temporary measure until we get headtracking for camera look, instead of mouse.

--- Experiments/ReplaceBuildings/Assets/Scripts/RayCast.cs
+++ Experiments/ReplaceBuildings/Assets/Scripts/RayCast.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class RayCast : MonoBehaviour {
-
+	static public GameObject target;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	void FixedUpdate () {
@@ -13,11 +13,12 @@ public class RayCast : MonoBehaviour {
 		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5f, 0));
 		if(Physics.Raycast(ray, out hit))
 		{
-			Debug.Log(hit.collider.tag);
+			target = hit.collider.gameObject;
+			//Debug.Log(hit.collider.gameObject.tag);
 		}
 		else
 		{
-			Debug.Log("Move along people, nothing to see here!");
+			//Debug.Log("Move along people, nothing to see here!");
 		}
 	}
 }
---
+++ Experiments/ReplaceBuildings/Assets/Scripts/ReplaceBuilding.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaceBuilding : MonoBehaviour {
+	static public bool selectingBuilding = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Space") && RayCast.target.tag == "Building")
+		{
+			selectingBuilding = true;
+			Debug.Log("selecting building");
+		}
+	}
+}
