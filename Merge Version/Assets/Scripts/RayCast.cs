31045bae5408dc6dd630a9a2d24a722ea1338a4d
Dave Bechtum <dbechtum@gmail.com>
2016-11-07 15:23:32 +0100

    Merged current progress into Merge Version

---
+++ Merge Version/Assets/Scripts/RayCast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+	static public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5f, 0));
+		if(Physics.Raycast(ray, out hit))
+		{
+			target = hit.collider.gameObject;
+			//Debug.Log(hit.collider.gameObject.tag);
+		}
+		else
+		{
+			//Debug.Log("Move along people, nothing to see here!");
+		}
+	}
+}
---
+++ Merge Version/Assets/Scripts/ReplaceBuilding.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaceBuilding : MonoBehaviour {
+	static public bool selectingBuilding = false;
+	public GameObject menu;
+	public GameObject boxButton;
+	public GameObject capsuleButton;
+	public GameObject ballButton;
+	public GameObject box;
+	public GameObject capsule;
+	public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		menu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(RayCast.target != null)
+		{
+			if(Input.GetButtonDown("Space") && RayCast.target.tag == "Building")
+			{
+				selectingBuilding = true;
+				menu.SetActive(true);
+				if(RayCast.target.name == "Cube")
+				{
+					boxButton.SetActive(false);
+					capsuleButton.SetActive(true);
+					ballButton.SetActive(true);
+				} else if (RayCast.target.name == "Capsule")
+				{
+					boxButton.SetActive(true);
+					capsuleButton.SetActive(false);
+					ballButton.SetActive(true);
+				} else if (RayCast.target.name == "Sphere")
+				{
+					boxButton.SetActive(true);
+					capsuleButton.SetActive(true);
+					ballButton.SetActive(false);
+				}
+				Debug.Log(RayCast.target.name);
+			}
+		}
+	}
+	public void makeBox()
+	{
+		Vector3 buildingPos = RayCast.target.transform.position;
+		Destroy(RayCast.target);
+		GameObject boxSpawn = (GameObject)Instantiate(box,buildingPos,Quaternion.identity);
+		boxSpawn.name = "Cube";
+		selectingBuilding = false;
+		menu.SetActive(false);
+	}
+	public void makeCapsule()
+	{
+		Vector3 buildingPos = RayCast.target.transform.position;
+		Destroy(RayCast.target);
+		GameObject capsuleSpawn = (GameObject)Instantiate(capsule,buildingPos,Quaternion.identity);
+		capsuleSpawn.name = "Capsule";
+		selectingBuilding = false;
+		menu.SetActive(false);
+	}
+	public void makeBall()
+	{
+		Vector3 buildingPos = RayCast.target.transform.position;
+		Destroy(RayCast.target);
+		GameObject ballSpawn = (GameObject)Instantiate(ball,buildingPos,Quaternion.identity);
+		ballSpawn.name = "Sphere";
+		selectingBuilding = false;
+		menu.SetActive(false);
+	}
+}
